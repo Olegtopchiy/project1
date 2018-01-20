ee1b5518d8c1a1c9c6403d3dce6523f440c4b9ea
OlegF1704 <dkakdw@gmail.com>
2018-01-20 13:53:13 +0300

    first try

--- Fib.php
+++ Fib.php
@@ -1,2 +1,13 @@
 <?php
-    
+function Fib ($sequence) {
+    if $sequence < 3 {
+        return $sequence;
+    }
+    else {
+        return Fib($sequence - 1) + Fib($sequence - 2);
+    }
+}
+
+for (i=1, i <=20; i++) {
+    echo fib($i);
+}
